08071a4953e18b19de0d2dc16bcd8ea93eab683e
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-05-25 16:43:55 +0300

    basic buttons logig added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let btn = document.getElementById('id-1');
+
+btn.onclick = function(event){
+    console.log(event);
+    console.log(this);
+    console.log(this.id.split('-')[1]); // tag id num
+};
+
+
+
+
+let checkBoxes = document.querySelectorAll('.check-box');
+
+for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].onclick = function(){
+       this.classList.toggle('check-box--checked');
+    };   
+}

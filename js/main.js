a8af5e293ae55e1730cec7724fa52835dd9bc36a
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2020-05-26 13:47:35 +0300

    input field complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let btn = document.getElementById('id-1');
+let btn = document.getElementById('menu_btn-1');
 
 btn.onclick = function(event){
     console.log(event);
@@ -16,5 +16,13 @@ let checkBoxes = document.querySelectorAll('.check-box');
 for (let i = 0; i < checkBoxes.length; i++) {
     checkBoxes[i].onclick = function(){
        this.classList.toggle('check-box--checked');
+
+       let currentID = this.id.split('-')[1];
+       let currentTextArea = document.getElementById(`text_area-${currentID}`);
+
+       currentTextArea.toggleAttribute('readonly');
     };   
 }
+
+
+// order change - document.getElementById("myRedDIV").style.order = "4";
